97e7a1ef30c6ef2f028edf6a7bf9d2fc711f9ef2
meeenakshii <meenakshisibyala@gmail.com>
2025-04-04 21:14:07 +0530

    second commit

--- budget_allocation/settings.py
+++ budget_allocation/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-2r@8w$0kk5%v0yb7sqk_gx0djb%do^b*8_q-3j=85!z!w(_^@7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['budget-buddy-web.onrender.com']  # Use '*' during development, but restrict it in production
+
 
 
 # Application definition
