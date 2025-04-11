0a1993f5133a7c5760888de46a6dc848fd4f2d95
meeenakshii <meenakshisibyala@gmail.com>
2025-04-11 09:51:40 +0530

    commit four

--- allocation/models.py
+++ allocation/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Expense(models.Model):
     category = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    date = models.DateField(auto_now_add=True)
+    date = models.DateField()
     description = models.TextField(blank=True, null=True)
 
     def __str__(self):
